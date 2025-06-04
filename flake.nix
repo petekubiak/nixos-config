21202c8e87a8c26600d6f1bc657b6a377ca6e88a
Pete Kubiak <peter.s.kubiak@bath.edu>
2025-06-04 10:50:41 +0100

    Initial NixOS configuration with flakes and home-manager

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "System configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.pete = ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
