3850cfaa50548bf6fa76418af2b4518a30dfcef5
Pete Kubiak <peter.s.kubiak@bath.edu>
2025-06-18 00:32:21 +0100

    Integrate stylix with waybar

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   programs.waybar = {
     enable = true;
     settings = {
@@ -30,7 +30,7 @@
         ];
 
         "cpu" = {
-          "interval" = 5;
+          "interval" = 1;
           "format" = "{load} {usage:>2}% {icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}{icon8}{icon9}{icon10}{icon11}{icon12}{icon13}{icon14}{icon15} {max_frequency} GHz ";
           "format-icons" = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
         };
@@ -119,35 +119,7 @@
       };
     };
 
-    style = ''
-      @define-color rosewater #f5e0dc;
-      @define-color flamingo #f2cdcd;
-      @define-color pink #f5c2e7;
-      @define-color mauve #cba6f7;
-      @define-color red #f38ba8;
-      @define-color maroon #eba0ac;
-      @define-color peach #fab387;
-      @define-color yellow #f9e2af;
-      @define-color green #a6e3a1;
-      @define-color teal #94e2d5;
-      @define-color sky #89dceb;
-      @define-color sapphire #74c7ec;
-      @define-color blue #89b4fa;
-      @define-color lavender #b4befe;
-      @define-color text #cdd6f4;
-      @define-color subtext1 #bac2de;
-      @define-color subtext0 #a6adc8;
-      @define-color overlay2 #9399b2;
-      @define-color overlay1 #7f849c;
-      @define-color overlay0 #6c7086;
-      @define-color surface2 #585b70;
-      @define-color surface1 #45475a;
-      @define-color surface0 #313244;
-      @define-color base #1e1e2e;
-      @define-color mantle #181825;
-      @define-color crust #11111b;
-      @define-color brown #561508;
-
+    style = lib.mkAfter ''
       * {
         font-family: "Sauce Code Pro Nerd Font";
         font-size: 15px;
@@ -155,11 +127,11 @@
 
       window#waybar {
         background: transparent;
-        color: @teal;
+        color: @base04;
         border-radius: 7rem;
         border-width: 2px;
         border-style: solid;
-        border-color: @teal;
+        border-color: @base04;
       }
 
       window#waybar.empty #window {
@@ -209,7 +181,7 @@
       #power-profiles-daemon:hover,
       #language:hover,
       #mpd:hover {
-          color: @green;
+          color: @base06;
       }
 
     #workspaces {
@@ -217,23 +189,23 @@
       background-color: transparent;
       /*border-width: 2px;
       border-style: solid;
-      border-color: @teal;*/
+      border-color: @base04;*/
     }
 
     #workspaces button {
-      color: @teal;
+      color: @base04;
       border-radius: 7rem;
     }
 
     #workspaces button.active {
       color: @brown;
-      background-color: @teal;
+      background-color: @base04;
       border-radius: 7rem;
     }
 
     #workspaces button:hover {
       color: @brown;
-      background-color: @green;
+      background-color: @base06;
       border-radius: 7rem;
     }
 
@@ -241,7 +213,7 @@
       border-radius: 7rem;
       padding: 0 10px;
       background-color: transparent;
-      color: @teal;
+      color: @base04;
       margin-left: 5px;
       margin-right: 5px;
     }
