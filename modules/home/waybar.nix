4712e95cae292b4be9defdda0323d557ebb731ad
Pete Kubiak <peter.s.kubiak@bath.edu>
2025-09-01 20:14:41 +0100

    Add current date to clock tooltip

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -32,7 +32,7 @@
         "cpu" = {
           "interval" = 1;
           "format" = "{load} {usage:>2}% {icon0}{icon1}{icon2}{icon3}{icon4}{icon5}{icon6}{icon7}{icon8}{icon9}{icon10}{icon11}{icon12}{icon13}{icon14}{icon15} {max_frequency} GHz ";
-          "format-icons" = ["▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
+          "format-icons" = [ "▁" "▂" "▃" "▄" "▅" "▆" "▇" "█" ];
         };
 
         "temperature" = {
@@ -71,7 +71,7 @@
           "tooltip" = false;
         };
 
-          "network" = {
+        "network" = {
           "format" = "{ifname}";
           "format-wifi" = "{icon} {signalStrength}%";
           "format-ethernet" = "󰈁";
@@ -109,9 +109,8 @@
         };
 
         "clock" = {
-          "tooltip-format" = "{calendar}";
+          "tooltip-format" = "{:%d/%m/%Y}\n\n{calendar}";
           "actions" = {
-            "on-click" = "mode";
             "on-scroll-up" = "shift_up";
             "on-scroll-down" = "shift_down";
           };
@@ -120,103 +119,103 @@
     };
 
     style = lib.mkAfter ''
-      * {
-        font-family: "Sauce Code Pro Nerd Font";
-        font-size: 15px;
+        * {
+          font-family: "Sauce Code Pro Nerd Font";
+          font-size: 15px;
+        }
+
+        window#waybar {
+          background: alpha(@base00, 0.5);
+          color: @base04;
+          border-radius: 7rem;
+          border-width: 2px;
+          border-style: solid;
+          border-color: @base04;
+        }
+
+        window#waybar.empty #window {
+          background-color: transparent;
+          padding: 0px;
+          border: 0px;
+        }
+
+        #clock,
+        #battery,
+        #cpu,
+        #memory,
+        #disk,
+        #temperature,
+        #backlight,
+        #network,
+        #pulseaudio,
+        #wireplumber,
+        #tray,
+        #bluetooth,
+        #mode,
+        #idle-inhibitor,
+        #scratchpad,
+        #power-profiles-daemon,
+        #language,
+        #mpd {
+          background-color: transparent;
+          padding: 0 10px;
+          border-radius: 7rem;
+        }
+
+        #clock:hover,
+        #battery:hover,
+        #cpu:hover,
+        #memory:hover,
+        #disk:hover,
+        #backlight:hover,
+        #network:hover,
+        #pulseaudio:hover,
+        #wireplumber:hover,
+        #custom-media:hover,
+        #tray:hover,
+        #mode:hover,
+        #bluetooth:hover,
+        #idle_inhibitor:hover,
+        #scratchpad:hover,
+        #power-profiles-daemon:hover,
+        #language:hover,
+        #mpd:hover {
+            color: @base06;
+        }
+
+      #workspaces {
+        border-radius: 7rem;
+        background-color: transparent;
+        /*border-width: 2px;
+        border-style: solid;
+        border-color: @base04;*/
       }
 
-      window#waybar {
-        background: alpha(@base00, 0.5);
+      #workspaces button {
         color: @base04;
         border-radius: 7rem;
-        border-width: 2px;
-        border-style: solid;
-        border-color: @base04;
       }
 
-      window#waybar.empty #window {
-        background-color: transparent;
-        padding: 0px;
-        border: 0px;
-      }
-
-      #clock,
-      #battery,
-      #cpu,
-      #memory,
-      #disk,
-      #temperature,
-      #backlight,
-      #network,
-      #pulseaudio,
-      #wireplumber,
-      #tray,
-      #bluetooth,
-      #mode,
-      #idle-inhibitor,
-      #scratchpad,
-      #power-profiles-daemon,
-      #language,
-      #mpd {
-        background-color: transparent;
-        padding: 0 10px;
+      #workspaces button.active {
+        color: @brown;
+        background-color: @base04;
         border-radius: 7rem;
       }
 
-      #clock:hover,
-      #battery:hover,
-      #cpu:hover,
-      #memory:hover,
-      #disk:hover,
-      #backlight:hover,
-      #network:hover,
-      #pulseaudio:hover,
-      #wireplumber:hover,
-      #custom-media:hover,
-      #tray:hover,
-      #mode:hover,
-      #bluetooth:hover,
-      #idle_inhibitor:hover,
-      #scratchpad:hover,
-      #power-profiles-daemon:hover,
-      #language:hover,
-      #mpd:hover {
-          color: @base06;
+      #workspaces button:hover {
+        color: @brown;
+        background-color: @base06;
+        border-radius: 7rem;
       }
 
-    #workspaces {
-      border-radius: 7rem;
-      background-color: transparent;
-      /*border-width: 2px;
-      border-style: solid;
-      border-color: @base04;*/
-    }
-
-    #workspaces button {
-      color: @base04;
-      border-radius: 7rem;
-    }
-
-    #workspaces button.active {
-      color: @brown;
-      background-color: @base04;
-      border-radius: 7rem;
-    }
-
-    #workspaces button:hover {
-      color: @brown;
-      background-color: @base06;
-      border-radius: 7rem;
-    }
-
-    #window {
-      border-radius: 7rem;
-      padding: 0 10px;
-      background-color: transparent;
-      color: @base04;
-      margin-left: 5px;
-      margin-right: 5px;
-    }
+      #window {
+        border-radius: 7rem;
+        padding: 0 10px;
+        background-color: transparent;
+        color: @base04;
+        margin-left: 5px;
+        margin-right: 5px;
+      }
     '';
   };
 }
