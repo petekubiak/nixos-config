942746a39758f57b8bfb9d079dccb698fd68ef17
Pete Kubiak <peter.s.kubiak@bath.edu>
2026-01-01 20:48:47 +0000

    Address deprecation warnings

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -214,7 +214,7 @@
 
   programs.hyprland = {
     enable = true;
-    package = inputs.hyprland.packages."${pkgs.system}".hyprland;
+    package = inputs.hyprland.packages."${pkgs.stdenv.hostPlatform.system}".hyprland;
   };
 
   programs.nh = {
--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,21 +1,16 @@
 { config, ... }: {
   programs.git = {
     enable = true;
-    aliases = {
-      graph = "log --oneline --all --graph --decorate";
-      wd = "diff --word-diff";
-      rl = "reflog --date=iso --pretty";
-      history = "blame -C -C -C";
-      co = "checkout";  
-    };
-    delta = {
-      enable = true;
-      options = {
-        side-by-side = true;
-        line-numbers = true;
+    settings = {
+      aliases = {
+        graph = "log --oneline --all --graph --decorate";
+        wd = "diff --word-diff";
+        rl = "reflog --date=iso --pretty";
+        history = "blame -C -C -C";
+        co = "checkout";
       };
-    };
-    extraConfig = {
+      user.email = "peter.s.kubiak@bath.edu";
+      user.name = "Pete Kubiak";
       gpg.format = "ssh";
       commit.gpgSign = true;
       init.defaultBranch = "main";
@@ -27,7 +22,13 @@
       rerere.enabled = true;
     };
     signing.key = "/home/pete/.ssh/id_ed25519.pub";
-    userEmail = "peter.s.kubiak@bath.edu";
-    userName = "Pete Kubiak";
+  };
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      side-by-side = true;
+      line-numbers = true;
+    };
   };
 }
